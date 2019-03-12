291318fe70cdbac46597f74d718e7b9e8be2a4c9
Lucas Rafael <joaolucasrafael@gmail.com>
2019-03-12 11:09:42 +0000

    Changes to Ray class.

--- RTX I/Ray.cpp
+++ RTX I/Ray.cpp
@@ -1,6 +1,11 @@
 #include "Ray.h"
 
-Ray::Ray(){
-
+Ray::Ray(){ // default constructor
+    this->_origin = new Vector3(0,0,0);
+    this->_direction = new Vector3(0,0,1);
+}
+Ray::Ray(float ox, float oy, float oz, float dx, float dy, float dz){
+    this->_origin = new Vector3(ox,oy,oz);
+    this->_direction = new Vector3(dx,dy,dz);
 }
 Ray::~Ray(){}
--- RTX I/Ray.h
+++ RTX I/Ray.h
@@ -4,11 +4,15 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include "Vector3.h"
 
 class Ray{
+private:
+    Vector3 *_origin, *_direction;
 public:
 
     Ray();
+    Ray(float ox, float oy, float oz, float dx, float dy, float dz);
     ~Ray();
 
 };
